9cf969d0c8a1e9d4ff57ec13b309fa7b193e00e4
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-20 23:40:20 +0600

    store client to github

--- src/pages/AvailableFood/AvailableFood.jsx
+++ src/pages/AvailableFood/AvailableFood.jsx
@@ -5,6 +5,7 @@ import AvailableFoodCard from "./AvailableFoodCard";
 import { AuthContext } from "../../providers/AuthProvider";
 import { useContext } from "react";
 import Loading from "../../shared/Loading";
+import { Button, Label, TextInput } from "flowbite-react";
 
 
 const AvailableFood = () => {
@@ -12,7 +13,7 @@ const AvailableFood = () => {
     // console.log(data);
     const { loading } = useContext(AuthContext);
     if (loading) {
-      return <Loading></Loading>;
+        return <Loading></Loading>;
     }
     // console.log(availableData)
     return (
@@ -23,8 +24,21 @@ const AvailableFood = () => {
             </Helmet>
             <div className="container mx-auto p-5">
                 <Title>Available Food</Title>
-                <div className="">
-                    <div></div>
+                <div className="flex">
+                    <div>
+                        <form className="flex max-w-md flex-row gap-4">
+                           
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email1" value="Your email" />
+                                </div>
+                                <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+                           
+                            <Button gradientDuoTone="purpleToBlue" type="submit">
+                                View Details
+                            </Button>
+                        </form>
+
+                    </div>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
 
